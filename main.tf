1b8fd733f6cc6a2231bef94d43e30668b0d62e21
Brent Daniel <brenthdaniel@gmail.com>
2023-05-06 18:48:06 -0700

    bug fixing

--- main.tf
+++ main.tf
@@ -45,7 +45,6 @@ module "autoscaling" {
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns = module.blog_alb.target_group_arns
   security_groups = [module.blog_sg.security_group_id]
-  subnet_id = module.blog_vpc.public_subnets[0]
   
   image_id           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
@@ -71,12 +70,6 @@ module "blog_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
     }
   ]
 
