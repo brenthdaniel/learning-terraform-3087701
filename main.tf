4500f108a56fae10391ee4067cb4f1acc087e5d9
Brent Daniel <brenthdaniel@gmail.com>
2023-05-06 18:14:57 -0700

    test

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.2"
-  name    = "blow_new"
+  name    = "blog"
 
   vpc_id              = module.vpc.public_subnets[0]
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
